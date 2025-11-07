4dff1de4ff97db0fed1ef0d462097944d9e86edc
pranavisda1 <pranavisda1@gmail.com>
2025-11-07 19:15:06 +0530

    3

--- src/utils/headphone-detector.js
+++ src/utils/headphone-detector.js
@@ -5,7 +5,7 @@ export async function detectHeadphones() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const audioOutputs = devices.filter(device => device.kind === 'audiooutput');
-    console.log(device)
+    console.log(mediaDevices)
     
     // Check if any output device label mentions headphones
     const hasHeadphones = audioOutputs.some(device => {
